a601edd768da2dc6b65d0af7923c574a417b0b86
Mihika Sarwate <mihika@example.com>
2026-09-24 23:56:24 +0530

    Deploy static site

---
+++ _next/static/chunks/0-i-lx42xkvok.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,676661,t=>{"use strict";var e=t.i(399354);t.s([],814485),t.i(814485),t.s(["default",()=>e.f],676661)}]);
